35d4ba100cb90a540ffb2077a489e34eafaffb32
smh-hosseiny <49597221+smh-hosseiny@users.noreply.github.com>
2021-05-28 21:26:39 +0430

    Update find_repeating_pattern.m

--- Method 1/Functions/find_repeating_pattern.m
+++ Method 1/Functions/find_repeating_pattern.m
@@ -1,11 +1,10 @@
 function [im, rep] = find_repeating_pattern(imgq,t)
- 
 Err = [];
 err_min = inf;
-for nang = [3 4 5 6]
-    imgq2 = imgq(:,[floor(t/nang)+1:end 1:floor(t/nang)],:);
-    err = mean(imgq2-imgq, 'all');
-    if err < err_min
+for nang = linspace(4, 6, 100)
+    imgq2 = imgq(:,[round(t/nang)+1:end 1:round(t/nang)],:);
+    err = immse(imgq(:,t/2-t/(2*nang):t/2+t/(2*nang),:), imgq2(:,t/2-t/(2*nang):t/2+t/(2*nang),:));
+    if err < err_mina
         err_min = err;
         imgt = imgq;
     end
@@ -15,6 +14,8 @@ end
 [~,id] = min(Err(:,2));
 rep = Err(id,1);
 im = imgt(:,t/2-t/(2*rep):t/2+t/(2*rep),:);
+rep = round(rep);
+% rep = round(t/size(im,2));
 
 
 end
